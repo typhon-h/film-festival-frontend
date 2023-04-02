77742a4754cbd40422432703704727ae8cc93556
Harrison Tyson <hty13@uclive.ac.nz>
2023-04-02 18:27:40 +1200

    Added Main layout

---
+++ src/layouts/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from "../components/Footer"
+import Navigation from "../components/Navigation"
+
+const Main = ({ children }: any) => {
+    return (
+        <div>
+            <Navigation />
+            <main>{children}</main>
+            <Footer />
+        </div>
+    )
+}
+
+export default Main;
