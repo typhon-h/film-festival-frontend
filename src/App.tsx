7f35e59e09ed46ed961ff5082717f3679fd68c70
Harrison Tyson <hty13@uclive.ac.nz>
2023-04-02 23:52:12 +1200

    Added component testing route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import Main from './layouts/Main';
+import FilmCard from './components/FilmCard';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Router>
           <div>
             <Routes>
+              <Route path="/component/test" element={<FilmCard title={"Test"} filmId={1} genreId={1} directorId={4} directorFirstName={'FName'} directorLastName={'LName'} releaseDate={'12-07-2002'} ageRating={'PG'} rating={6.33} />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
