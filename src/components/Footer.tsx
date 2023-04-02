12e0918a351eb9837500bff4fdea87182b320f22
Harrison Tyson <hty13@uclive.ac.nz>
2023-04-02 17:55:41 +1200

    Added initial Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,25 @@
+const Footer = () => {
+
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-bottom">
+                <div className="container">
+                    <ul className="navbar-nav w-100 mt-2 mt-lg-0 d-flex justify-content-md-between">
+                        <li className="nav-item">
+                            <p className="nav-link disabled">SENG365 2023 - Film Festival</p>
+                        </li>
+
+                        <li className="nav-item">
+                            <p className="nav-link disabled">Harrison Tyson / hty13 / 12577377</p>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+        </div>
+    )
+
+}
+
+
+export default Footer;
