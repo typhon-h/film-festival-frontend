18c94fba88821973d1034f4f52602a1485ed255d
Harrison Tyson <hty13@uclive.ac.nz>
2023-04-07 10:39:18 +1200

    Added scroll to top button to whole site

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import Main from './layouts/Main';
 import ViewFilms from './pages/ViewFilms';
+import React from 'react';
 
 function App() {
+  const ref = React.useRef<HTMLButtonElement>(null)
+
+
+  const toggle_return_to_top = () => {
+    if (ref.current) {
+      if (document.documentElement.scrollTop > 50 || document.body.scrollTop > 50) {
+        ref.current.style.display = "flex";
+      } else {
+        ref.current.style.display = "none";
+      }
+    }
+  }
+
+  window.addEventListener("scroll", toggle_return_to_top)
+
+
   return (
     <div className="App">
       <Router>
@@ -16,6 +33,10 @@ function App() {
               <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
+
+          {/* Return to top button */}
+          <button ref={ref} onClick={() => { document.documentElement.scrollTop = 0; document.body.scrollTop = 0; }} className='btn btn-light justify-content-center fixed-bottom fs-1 border m-4 col-2 col-md-1 m-md-5' style={{ display: 'none', left: 'auto' }}><i className="bi bi-caret-up-fill"></i></button>
+
         </Main>
       </Router>
     </div >
