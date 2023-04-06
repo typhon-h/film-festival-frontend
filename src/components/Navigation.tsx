b949a220494e97dfa7f16f25f319776b288e4a8a
Harrison Tyson <hty13@uclive.ac.nz>
2023-04-07 10:58:10 +1200

    Removed unused parent div from nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,31 +4,29 @@ const Navigation = () => {
     const location = useLocation();
 
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-                <div className="container">
-                    <a className="navbar-brand" href="/">Film Festival</a>
-                    <button className="navbar-toggler hidden-lg-up" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                        aria-expanded="false" aria-label="Toggle navigation"><i className="bi bi-list"></i></button>
-                    <div className="collapse navbar-collapse" id="collapsibleNavId">
-                        <ul className="navbar-nav me-auto mt-2 mt-lg-0">
-                            <li className="nav-item">
-                                <a className={'nav-link ' + (location.pathname === '/' ? 'active' : '')} href="/" aria-current="page">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className={'nav-link ' + (location.pathname === '/films' ? 'active' : '')} href="/films" aria-current="page">Films</a>
-                            </li>
-                        </ul>
+        <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light ">
+            <div className="container">
+                <a className="navbar-brand" href="/">Film Festival</a>
+                <button className="navbar-toggler hidden-lg-up" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                    aria-expanded="false" aria-label="Toggle navigation"><i className="bi bi-list"></i></button>
+                <div className="collapse navbar-collapse" id="collapsibleNavId">
+                    <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+                        <li className="nav-item">
+                            <a className={'nav-link ' + (location.pathname === '/' ? 'active' : '')} href="/" aria-current="page">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className={'nav-link ' + (location.pathname === '/films' ? 'active' : '')} href="/films" aria-current="page">Films</a>
+                        </li>
+                    </ul>
 
-                        <form action="/films" className="d-flex flex-column flex-lg-row form-inline my-2 my-lg-0">
-                            <input name="q" className="form-control mb-1 mb-lg-0 me-lg-1" type="search" placeholder="Search Film" aria-label="Search" maxLength={64} />
-                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                        </form>
-                    </div>
+                    <form action="/films" className="d-flex flex-column flex-lg-row form-inline my-2 my-lg-0">
+                        <input name="q" className="form-control mb-1 mb-lg-0 me-lg-1" type="search" placeholder="Search Film" aria-label="Search" maxLength={64} />
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
                 </div>
-            </nav>
-
-        </div>)
+            </div>
+        </nav>
+    )
 }
 
 
