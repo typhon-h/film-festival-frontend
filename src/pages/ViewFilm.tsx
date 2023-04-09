2f02a2c11035a8652a774f19c26b344103dd01db
Harrison Tyson <hty13@uclive.ac.nz>
2023-04-09 21:50:54 +1200

    Added Similar FIlms to view film page

--- src/pages/ViewFilm.tsx
+++ src/pages/ViewFilm.tsx
@@ -7,6 +7,7 @@ import default_film_picture from "../assets/default_film_picture.png";
 import { Buffer } from "buffer";
 import ReviewsPanel from "../components/ReviewsPanel"
 import ViewFilmPlaceholder from "./placeholder/ViewFilmPlaceholder"
+import SimilarFilms from "../components/SimilarFilms"
 
 
 
@@ -108,7 +109,7 @@ const ViewFilm = (props: any) => {
 
     const film_details = (film: Film) => {
         return (
-            <div className='d-flex flex-column justify-content-center bg-light col-12 col-sm-5 col-lg-12 col-xxl-3 border rounded-3 p-3 my-sm-0 my-3 me-xxl-5'>
+            <div className='d-flex flex-column justify-content-center bg-light col-12 col-sm-5 col-lg-12 col-xxl-12 border rounded-3 p-3 my-sm-0 my-3 me-xxl-5'>
                 <h3 className='fs-4 text-decoration-underline'>Details</h3>
                 <div className='d-flex flex-column justify-content-start'>
                     <div className='d-flex flex-row justify-content-between'>
@@ -181,21 +182,21 @@ const ViewFilm = (props: any) => {
     }
 
     return (
-        <div className="d-flex flex-column align-items-start align-items-sm-center p-4 placeholder-glow">
+        <div className="d-flex flex-column align-items-center align-items-sm-center p-4 placeholder-glow">
             {(timedOut) ? error_timed_out() : ''}
             {(errorFlag) ? error_unexpected() : ''}
             <h1 className='fs-1 text-secondary align-self-center mb-3'>{film.title}</h1>
-            <div className='d-flex flex-column col-12 flex-lg-row flex-xxl-column align-items-center justify-content-lg-between justify-content-xxl-center'>
-                <div className='d-flex flex-column col-12 col-sm-10 col-lg-4 col-xxl-3 mb-3 align-items-center me-xxl-5'>
+            <div className='d-flex flex-column col-12 flex-lg-row  align-items-center justify-content-lg-between justify-content-xxl-center'>
+                <div className='d-flex flex-column col-12 col-sm-10 col-lg-4 col-xxl-4 mb-3 align-items-center me-xxl-5'>
                     <div className='d-flex flex-column align-self-center col-sm-10 col-lg-12 mb-3 mb-sm-0'>
                         <img className={'img-fluid img-thumbnail ' + ((!heroImageLoaded) ? 'placeholder' : '')} src={(heroImageLoaded ? heroImage : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNgYAAAAAMAASsJTYQAAAAASUVORK5CYII=")} alt="Film Hero" />
                     </div>
                 </div>
-                <div className='d-flex flex-column flex-sm-row-reverse flex-lg-column flex-xxl-row-reverse col-12 col-lg-7 col-xxl-12 justify-content-sm-around justify-content-xxl-center align-items-center m-sm-3 mt-lg-0'>
-                    <div className='d-flex flex-column col-12 col-sm-6 col-lg-12 col-xxl-4'>
+                <div className='d-flex flex-column flex-sm-row-reverse flex-lg-column col-12 col-lg-7 col-xxl-7 justify-content-sm-around justify-content-xxl-center align-items-center m-sm-3 mt-lg-0'>
+                    <div className='d-flex flex-column col-12 col-sm-6 col-lg-12 col-xxl-12'>
                         <p className="d-inline-block col-12 text-dark text-start text-wrap text-break"><b>Description: </b>{film.description}</p>
 
-                        <div className='d-flex flex-column col-12 col-md-8 col-lg-5 col-xl-5 col-xxl-6 mb-3'>
+                        <div className='d-flex flex-column col-12 col-md-8 col-lg-5 col-xl-5 col-xxl-4 mb-3'>
                             <p className='align-self-start fw-bold'>Director:</p>
                             <div className='bg-light rounded-3'>
                                 <DirectorCard director={{ id: film.directorId, firstName: film.directorFirstName, lastName: film.directorLastName }} />
@@ -206,9 +207,16 @@ const ViewFilm = (props: any) => {
                     {film_details(film)}
                 </div>
             </div>
-            <div className='d-flex flex-column col-12 col-xxl-8'>
+
+            <div className='d-flex flex-column mb-4 col-12 col-xxl-8'>
                 <ReviewsPanel filmId={filmId} rating={film.rating} />
             </div>
+
+            <div className='d-flex flex-column col-12 col-md-5 col-lg-8 col-xxl-6'>
+                <h3>You may also enjoy</h3>
+                <SimilarFilms filmId={film.filmId} directorId={film.directorId} genreId={film.genreId} />
+            </div>
+
         </div >
     )
 
