7c08ad63943a204aaf041d44c274d8b7493748ad
Harrison Tyson <hty13@uclive.ac.nz>
2023-04-10 04:50:24 +1200

    Added button to home page to direct to Register

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+
+const Home = () => {
+
+    return (
+        <button onClick={() => window.location.href = '/register'} type="button" className="btn btn-outline-primary btn-lg w-50">Register</button>
+    )
+}
+
+export default Home
