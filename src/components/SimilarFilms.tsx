1fc9ec18fafabe85c2646efcfdd4e0f1db3928c7
Harrison Tyson <hty13@uclive.ac.nz>
2023-04-09 22:12:22 +1200

    Made carousel buttons circular

--- src/components/SimilarFilms.tsx
+++ src/components/SimilarFilms.tsx
@@ -71,11 +71,11 @@ const SimilarFilms = (props: any) => {
 
             </div >
             <button className="carousel-control-prev" type="button" data-bs-target="#similarFilms" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
+                <span className="carousel-control-prev-icon bg-dark rounded-circle" aria-hidden="true"></span>
                 <span className="visually-hidden">Previous</span>
             </button>
             <button className="carousel-control-next" type="button" data-bs-target="#similarFilms" data-bs-slide="next">
-                <span className="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
+                <span className="carousel-control-next-icon bg-dark rounded-circle" aria-hidden="true"></span>
                 <span className="visually-hidden">Next</span>
             </button>
         </div >
