cafcfedd347df8d74916d2c458a480b158f33b01
Harrison Tyson <hty13@uclive.ac.nz>
2023-04-10 04:49:54 +1200

    Added home page and register path

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Main from './layouts/Main';
 import ViewFilms from './pages/ViewFilms';
 import ViewFilm from './pages/ViewFilm';
 import SimilarFilms from './components/SimilarFilms';
+import Home from './pages/Home';
+import Register from './pages/Register';
 
 function App() {
   const ref = React.useRef<HTMLDivElement>(null)
@@ -30,6 +32,9 @@ function App() {
         <Main>
           <div style={{ minHeight: '85vh' }}>
             <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/home" element={<Home />}></Route>
+              <Route path="/register" element={<Register />}></Route>
               <Route path="/films" element={<ViewFilms />}></Route>
               <Route path="/films/:filmId" element={<ViewFilm />}></Route>
               <Route path="/component/test" element={<SimilarFilms filmId={1} directorId={2} genreId={2} />} />
