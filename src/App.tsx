8129f066c2b305942b49e43da79308ff43a4789e
Harrison Tyson <hty13@uclive.ac.nz>
2023-04-11 10:30:11 +1200

    Renamed scroll button ref

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,15 @@ import Home from './pages/Home';
 import Register from './pages/Register';
 
 function App() {
-  const ref = React.useRef<HTMLDivElement>(null)
+  const scrollButton = React.useRef<HTMLDivElement>(null)
 
 
   const toggle_return_to_top = () => {
-    if (ref.current) {
+    if (scrollButton.current) {
       if (document.documentElement.scrollTop > 50 || document.body.scrollTop > 50) {
-        ref.current.style.display = "flex";
+        scrollButton.current.style.display = "flex";
       } else {
-        ref.current.style.display = "none";
+        scrollButton.current.style.display = "none";
       }
     }
   }
@@ -43,7 +43,7 @@ function App() {
           </div>
 
           {/* Return to top button */}
-          <div ref={ref} className='justify-content-center fixed-bottom m-4 m-md-5' style={{ display: 'none', left: 'auto' }}>
+          <div ref={scrollButton} className='justify-content-center fixed-bottom m-4 m-md-5' style={{ display: 'none', left: 'auto' }}>
 
             <span role={'button'} onClick={() => { document.documentElement.scrollTop = 0; document.body.scrollTop = 0; }} className='fs-1 text-info p-0'><i className="bi bi-arrow-up-circle-fill "></i></span>
           </div>
