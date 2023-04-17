31c152aa194725cbb8c32695def469e954fbac2e
Harrison Tyson <hty13@uclive.ac.nz>
2023-04-17 16:32:42 +1200

    Updated restricted to have full width for easier styling

--- src/layouts/Restricted.tsx
+++ src/layouts/Restricted.tsx
@@ -9,7 +9,7 @@ const Restricted = (props: any) => {
 
     if ((props.whitelist && props.whitelist.includes(parseInt(activeUser, 10))) || (!props.whitelist && !props.blacklist && authenticated && activeUser) || (props.blacklist && !props.blacklist.includes(parseInt(activeUser, 10))) || (!props.blacklist && !props.whitelist && !authenticated && !activeUser)) {
         return (
-            <div>
+            <div className='w-100'>
                 {props.children as JSX.Element}
             </div>
         )
