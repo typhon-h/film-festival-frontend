cc56134424cc74bc71cdd56ca96fea2f6304d022
Harrison Tyson <hty13@uclive.ac.nz>
2023-04-17 18:35:00 +1200

    Added User type

---
+++ src/types/users.d.ts
@@ -0,0 +1,18 @@
+type User = {
+    /**
+     * User id as defined by the database
+     */
+    id: number,
+    /**
+     * User email as defined when created
+     */
+    email: string,
+    /**
+     * User first name as defined when created
+     */
+    first_name: string,
+    /**
+     * User last name as defined when created
+     */
+    last_name: string
+}
